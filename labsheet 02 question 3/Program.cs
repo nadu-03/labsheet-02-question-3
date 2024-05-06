45e66c9614ea21a98e0a532cd8500ecf009e2a97
nadu <nadu@WINDOWS-UINETAB>
2024-05-06 03:54:52 -0700

    Add project files.

---
+++ labsheet 02 question 3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_Q3
+{
+    public class TemperatureTracker
+    {
+        private double[] temperature = new double[7];
+
+        public void dailyTemperature()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write($"Enter temperature for day {i + 1}: ");
+                Temperature[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        public void Report()
+        {
+            Console.WriteLine("\nWeekly Temperature Report:");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {temperatures[i]}°C");
+            }
+        }
+    }
+}
+}
